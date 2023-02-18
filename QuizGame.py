92d6cdec3865c56ffd2df379bec9c4cefd0afc41
Frank Din <frankdin@yahoo.com>
2023-02-18 01:24:48 -0500

    Created an multidimensional array containing several arrays of different subject areas.

--- QuizGame.py
+++ QuizGame.py
@@ -1,17 +1,4 @@
-qandA = [
-    ['1 + 1', 2, [2, 4, 6, 8]], 
-    ['64 * 64', 4096, [100, 3000, 1500, 4096]], 
-    ['15000 - 27', 14973, [15100, 14500, 2, 14973]], 
-    ['32800 / 8', 4100, [2000, 400, 5, 4100]]
-    ]
-exit = False
-points = 0
-right_ans = 0
-while (exit == False):
-    print("***WHO WANTS TO BE A MILLIONAIRE***")
-    user_in = ''
-    print('Type "EXIT" to leave game.')
-    print('To answer the question, enter the position of the answer in the array.\nFor example if the answer array is [2, 4, 6, 8] and the answer is 4, type 2.')
+def quiz(qandA, exit, points, right_ans, user_in):    
     for i in range(len(qandA)):
 
         #this stores the index of the right answer plus 1
@@ -28,21 +15,78 @@ while (exit == False):
         if (user_in.lower() == 'exit'):
             exit = True
             print('Bye!')
-        
+            
         #we compare the user input to the string version of right_ans
         if (user_in == str(right_ans)):
             points += 1
             print('Correct | Points: {points}\n'.format(points = points))
         else:
             print('Incorrect | Points: {points}\n'.format(points = points))
-    
+        
+        print(points/len(qandA)*100)
+        
     #this outputs the reults of the quiz
-    if (points/len(qandA)*100 >= 90):
+    results(qandA, points)
+
+def results(qandA, points):
+    if (points/len(qandA)*100 >= 90.0):
         print('Congrats, you got an A')
-    elif (points/len(qandA)*100 >= 80):
+    elif (points/len(qandA)*100 >= 80.0):
         print('Congrats, you got an B')
-    elif (points/len(qandA)*100 >= 70):
+    elif (points/len(qandA)*100 >= 70.0):
         print('You got an C. You got study a little harder.')
-    elif (points/len(qandA)*100 < 70):
+    elif (points/len(qandA)*100 < 70.0):
         print("It seems like you didn't study at all. Try again next time.")
-    exit = True
+
+
+subjects = [
+    #Math
+    [
+        ['1 + 1', 2, [2, 4, 6, 8]], 
+        ['64 * 64', 4096, [100, 3000, 1500, 4096]], 
+        ['15000 - 27', 14973, [15100, 14500, 14973, 2]], 
+        ['32800 / 8', 4100, [2000, 400, 5, 4100]]
+    ],
+    
+    #History
+    [
+        ['When was Malcom X born?', 'May 19, 1925', ['Jan 20 1930', 'March 18, 1925', 'May 19, 1925', 'May 19, 1923']], 
+        ['When did Nelson Mandela die?', 'Dec 05, 2013', ['Dec 05, 2013', 'Jan 20, 2000', 'July 10, 2012', 'Dec, 05 2014']], 
+        ['What year was Barack Obama first elected President?', 2008, [2012, 2008, 2007, 1990]], 
+        ['What year did the U.S get into WWII?', 1941, [1942, 1921, 1941, 1940]]
+    ],
+
+    #Geography
+    [
+        ['How many countries are there in Africa?', 54, [50, 54, 56, 58]], 
+        ['What is the capital of Russia?', 4096, [100, 3000, 1500, 4096]], 
+        ['What ocen is between Africa and America?', 14973, [15100, 14500, 14973, 2]], 
+        ['What continent is at the North Pole?', 4100, [2000, 400, 5, 4100]]
+    ],
+
+    #Political Science
+    [
+        ['How many senators does the US have?', 100, [102, 100, 106, 50]], 
+        ['Who is the President of South Africa?', 'Cyril Ramaphosa', ['Nelson Mandela', 'Paul Biya', 'George Washington', 'Cyril Ramaphosa']], 
+        ['When did Nigeria get its independence?', 'October 1, 1960', ['October 1, 1960', 'October 2, 1960', 'October 3, 1960', 'October 4, 1960']], 
+        ['Who is the Mayor of Atlanta, Georgia?', 'Andre Dickens', ['Jason Black', 'Joe Biden', 'Andre Dickens', 'Tyler Perry']]
+    ]
+]
+
+exit = False
+score = 0
+correct_ans = 0
+quiz_question_answer = ''
+subject_choice = ''
+
+print(subjects[0])
+while (exit == False):
+    print('***********************************')
+    print("***WHO WANTS TO BE A MILLIONAIRE***")
+    print('***********************************')
+    print('Type "EXIT" to leave game.')
+    print('To answer the question, enter the position of the answer in the array.\nFor example if the answer array is [2, 4, 6, 8] and the answer is 4, type 2.')
+    # subject_choice = int(input('Choose your subject area: Math, Geography, Political Science, History: '))
+    # # print(subject_choice)
+    # # quiz(subjects[subject_choice] + 1, exit, score, correct_ans)
+    # # # exit = True

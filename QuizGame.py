d7f5959993b9a5dc00368c2c138ae47a2b061b43
Frank Din <frankdin@yahoo.com>
2023-02-18 02:34:24 -0500

    Added feature to enable user to exit the current quiz without exiting the whole game.

--- QuizGame.py
+++ QuizGame.py
@@ -12,12 +12,12 @@ def results(qandA, points):
 #this is the condition to end the game prematurely
 def exit_condition(user_in):
     if (user_in.lower() == 'exit'):
-            exit = True
             print('Bye!')
-    #return
+            return True
         
-def quiz(qandA, exit, points, right_ans, user_in):    
-    while(exit == False):
+def quiz(qandA, inner_exit, points, right_ans, user_in):    
+    while(inner_exit == False):
+        print("At any time, you can type 'EXIT' to return to the main menu.")
         for i in range(len(qandA)):
 
             #this stores the index of the right answer plus 1
@@ -30,19 +30,16 @@ def quiz(qandA, exit, points, right_ans, user_in):
             #this is the statement requiring user input
             user_in = input('{question}:\n{answer_options}\nAnswer: '.format(question = qandA[i][0], answer_options = qandA[i][2]))
             
-            #this is the condition to end the game prematurely
-            exit_condition(user_in, exit)
-                
             #we compare the user input to the string version of right_ans
-            if (user_in == str(right_ans)):
+            if (user_in.lower() == 'exit'):
+                print('Exiting to main menu!')
+                return
+            elif (user_in == str(right_ans)):
                 points += 1
                 print('Correct | Points: {points}\n'.format(points = points))
             else:
                 print('Incorrect | Points: {points}\n'.format(points = points))
-            
-            print(points/len(qandA)*100)
-        
-    results(qandA, points)
+        results(qandA, points)
 
 subjects = [
     #Math
@@ -78,14 +75,15 @@ subjects = [
     ]
 ]
 
-exit = False
+inner_exit = False
+outer_exit = False
 score = 0
 correct_ans = 0
 user_ans = ''
 subject_choice = ''
 
 print(subjects[0])
-while (exit == False):
+while (outer_exit == False):
     print('***********************************')
     print("***WHO WANTS TO BE A MILLIONAIRE***")
     print('***********************************')
@@ -93,5 +91,5 @@ while (exit == False):
     print('To answer the question, enter the position of the answer in the array.\nFor example if the answer array is [2, 4, 6, 8] and the answer is 4, type 2.')
     subject_choice = int(input('Choose your subject area: Math, History, Geography, Political Science: '))
     #exit_condition(str(subject_choice, exit))
-    quiz(subjects[subject_choice - 1], exit, score, correct_ans, user_ans)
+    quiz(subjects[subject_choice - 1], inner_exit, score, correct_ans, user_ans)
     # exit = True

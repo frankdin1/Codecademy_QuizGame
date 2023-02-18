561a5dc2d0b0dfe917585cef805715139aba4586
Frank Din <frankdin@yahoo.com>
2023-02-17 23:30:27 -0500

    Added comments.

--- QuizGame.py
+++ QuizGame.py
@@ -8,23 +8,35 @@ exit = False
 points = 0
 right_ans = 0
 while (exit == False):
+    print("***WHO WANTS TO BE A MILLIONAIRE***")
     user_in = ''
     print('Type "EXIT" to leave game.')
+    print('To answer the question, enter the position of the answer in the array.\nFor example if the answer array is [2, 4, 6, 8] and the answer is 4, type 2.')
     for i in range(len(qandA)):
+
+        #this stores the index of the right answer plus 1
         if (qandA[i][1] in qandA[i][2]):
             right_ans = qandA[i][2].index(qandA[i][1]) + 1
+
+        #this tells you the question number
+        print("Question {question_number}".format(question_number = i + 1))
+
+        #this is the statement requiring user input
         user_in = input('{question}:\n{answer_options}\nAnswer: '.format(question = qandA[i][0], answer_options = qandA[i][2]))
-        #print('\n****************** \n1\t|\t2 \n****************** \n3\t|\t4\n******************\nAnswer: ')
+        
+        #this is the condition to end the game prematurely
         if (user_in.lower() == 'exit'):
             exit = True
             print('Bye!')
-#user_in == str(qandA[i][1])
+        
+        #we compare the user input to the string version of right_ans
         if (user_in == str(right_ans)):
             points += 1
-            print('Correct | Points: {points}'.format(points = points))
+            print('Correct | Points: {points}\n'.format(points = points))
         else:
-            print('Incorrect | Points: {points}'.format(points = points))
+            print('Incorrect | Points: {points}\n'.format(points = points))
     
+    #this outputs the reults of the quiz
     if (points/len(qandA)*100 >= 90):
         print('Congrats, you got an A')
     elif (points/len(qandA)*100 >= 80):
